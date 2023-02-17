b39f6cb693495ba823ae038b8de9f53a8f33e848
ccumaco <cumacopesca.5@nlsa.teleperformance.com>
2023-02-17 12:21:43 -0500

    new files images

--- src/views/emails.vue
+++ src/views/emails.vue
@@ -74,7 +74,7 @@ export default defineComponent({
             <i class='pi pi-arrow-left mr-2 '></i> Volver
         </router-link>
       <h2 class="container-social__content--title mt-3">
-        <img src="/public/images/email.png" alt="pencil" class='mr-2'> Crea un correo electrónico
+        <img src="/images/email.png" alt="pencil" class='mr-2'> Crea un correo electrónico
       </h2>
       <p class="container-social__content--description">
         Con esta plantilla genera borradores completos de correos electrónicos en cuestión de segundos, ahorra tiempo de redacción y asegúrate que tu comunicación sea clara y precisa.
--- src/views/free-style.vue
+++ src/views/free-style.vue
@@ -74,7 +74,7 @@ export default defineComponent({
             <i class='pi pi-arrow-left mr-2 '></i> Volver
         </router-link>
       <h2 class="container-social__content--title mt-3">
-        <img src="/public/images/pencil.png" alt="pencil" class='mr-2'> Estilo libre
+        <img src="images/pencil.png" alt="pencil" class='mr-2'> Estilo libre
       </h2>
       <p class="container-social__content--description">
         Redacta con ayuda de la IA de manera flexible para crear cualquier contenido escrito que necesites

7cf0cd247707b63ef0fea8e7726fec531d167202
ccumaco <cumacopesca.5@nlsa.teleperformance.com>
2023-04-23 22:04:50 -0500

    new footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,12 +7,12 @@
                 </router-link>
                 <div class="container-info">
                     <p class='contact '>Contacto</p>
-                    <a class='' aria-label="Chat on WhatsApp" href="https://wa.me/+573132777667?text=saludando desde arquitext" target='_blank'> <i class='pi pi-whatsapp mr-1'></i>313 277 7667</a>
-                    <a class='' href="mailto:macgraficas1267@gmail.com"><i class='pi pi-envelope mr-1'></i>macgraficas1267@gmail.com</a>
+                    <a class='' aria-label="Chat on WhatsApp" href="https://wa.me/+573132777667?text=Hola tengo dudas sobre sus productos" target='_blank'> <i class='pi pi-whatsapp mr-1'></i>313 277 7667</a>
+                    <a class='' href="mailto:contacto@arquitext.com.co"><i class='pi pi-envelope mr-1'></i>contacto@arquitext.com.co</a>
                 </div>
             </div>
             <div class="container-bottom text-center">
-                © Architext. Todos los derechos reservados
+                © Arqhitext. Todos los derechos reservados
             </div>
         </div>
     </div>
